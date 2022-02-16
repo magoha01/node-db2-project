73cdfd6dfc18a6b92f236973c2bfd469dbe964f7
Hailey Magoline <magoline.hailey@gmail.com>
2022-02-16 09:41:01 -0600

    test push

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,34 +1,31 @@
 const Cars = require("./cars-model");
 
 const checkCarId = async (req, res, next) => {
-    try {
-      const cars = await Cars.getById(req.params.id) 
-      if(!cars) {
-        next({ status: 404, message: "account not found"});
-      } else {
-        req.cars = cars;
-        next();
-      }
-    } catch(err) {
-      next(err)
+  try {
+    const { id } = req.params;
+    const cars = await Cars.getById(id);
+    if (!cars) {
+      res.status(404).json({
+        message: `car with id ${id} is not found`,
+      });
+    } else {
+      req.cars = cars;
+      next();
     }
-}
+  } catch (err) {
+    next(err);
+  }
+};
 
-const checkCarPayload = (req, res, next) => {
-  
-}
+const checkCarPayload = (req, res, next) => {};
 
-const checkVinNumberValid = (req, res, next) => {
-  
-}
+const checkVinNumberValid = (req, res, next) => {};
 
-const checkVinNumberUnique = (req, res, next) => {
-  
-}
+const checkVinNumberUnique = (req, res, next) => {};
 
 module.exports = {
-  checkCarId, 
+  checkCarId,
   checkCarPayload,
   checkVinNumberUnique,
-  checkVinNumberValid
-}
+  checkVinNumberValid,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,19 +1,35 @@
 const Cars = require("./cars-model");
 const router = require("express").Router();
 
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberId,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
+
 router.get("/", (req, res, next) => {
   Cars.getAll().then((cars) => {
     res.status(200).json(cars);
   });
 });
 
-router.get("/", (req, res, next) => {
-    res.json(req.cars)
+router.get("/", checkCarId, (req, res, next) => {
+  res.json(req.cars);
 });
 
-router.post("/", (req, res, next) => {
-//   console.log("new car");
-//   next();
-});
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberId,
+  checkVinNumberUnique,
+  (req, res, next) => {
+   Cars.insert(req.body)
+   .then((newCar) => {
+       res.status(201).json(newCar)
+   })
+   .catch(next)
+  }
+);
 
 module.exports = router;
